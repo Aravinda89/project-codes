a8549af39a11447184edeae0cefa8aba65001f61
Aravinda <31471559+Aravinda89@users.noreply.github.com>
2019-01-13 13:47:44 +0800

    Update and rename keras_test1.py to use_keras_pretrained_models.py
    Edited

--- use_keras_pretrained_models.py
+++ use_keras_pretrained_models.py
@@ -1,11 +1,5 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-This is a temporary script file.
-"""
-
-## using Keras pretrained model
+## using Keras pretrained models
 
 import keras
 import numpy as np
@@ -28,8 +22,6 @@ from keras.applications.imagenet_utils import decode_predictions
 import matplotlib.pyplot as plt
 #matplotlib inline
 
-#filename = 'D:/AG/Keras/audi_PNG1733.png'
-#filename = 'D:/AG/Keras/plate.jfif'
 filename = 'D:/AG/Keras/Lamborghini.jpg'
 
 # load an image in PIL format
@@ -46,10 +38,7 @@ plt.imshow(np.uint8(numpy_image))
 plt.show()
 print('numpy array size',numpy_image.shape)
  
-# Convert the image / images into batch format
-# expand_dims will add an extra dimension to the data at a particular axis
-# We want the input matrix to the network to be of the form (batchsize, height, width, channels)
-# Thus we add the extra dimension to the axis 0.
+# Convert the image  into batch format
 image_batch = np.expand_dims(numpy_image, axis=0)
 print('image batch size', image_batch.shape)
 plt.imshow(np.uint8(image_batch[0]))
